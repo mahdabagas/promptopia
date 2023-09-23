4613d476eacbe06251041af2e18ee8058429ec28
Bagas Mahda Dhani <muksinsaja12@gmail.com>
2023-09-24 01:20:33 +0700

    Create Feed Page

--- models/prompt.js
+++ models/prompt.js
@@ -1,4 +1,4 @@
-import { Schema, model, models } from "mongoose";
+import { Schema, model, models } from 'mongoose';
 
 const PromptSchema = new Schema({
     creator: {
@@ -15,6 +15,6 @@ const PromptSchema = new Schema({
     }
 });
 
-const Prompt = models.prompt || model("Prompt", PromptSchema);
+const Prompt = models.Prompt || model('Prompt', PromptSchema);
 
 export default Prompt;
