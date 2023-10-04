2639c63dbfc9f36fe3dd75806a1edc9d1afc2c71
Bagas Mahda Dhani <muksinsaja12@gmail.com>
2023-10-04 21:16:26 +0700

    Profile page

--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -1,24 +1,29 @@
-"use client"
+"use client";
 
-import { useState } from "react"
-import Image from "next/image"
-import { useSession } from "next-auth/react"
-import { usePathname, useRouter } from "next/navigation"
+import { useState } from "react";
+import Image from "next/image";
+import { useSession } from "next-auth/react";
+import { usePathname, useRouter } from "next/navigation";
 
-const PromptCard = ({post, handleEdit, handleDelete, handleTagClick}) => {
+const PromptCard = ({ post, handleEdit, handleDelete, handleTagClick }) => {
+  const { data: session } = useSession();
+  const pathname = usePathname();
+  const router = useRouter();
+
   const [copied, setCopied] = useState("");
 
   const handleCopy = () => {
     setCopied(post.prompt);
     navigator.clipboard.writeText(post.prompt);
     setTimeout(() => setCopied(false), 3000);
-  }
+  };
 
   return (
     <div className="prompt_card">
       <div className="flex justify-between items-start gap-5">
         <div className="flex-1 flex justify-start items-center gap-3 cursor-pointer">
-          <Image src={post.creator.image}
+          <Image
+            src={post.creator.image}
             alt="user_image"
             width={40}
             height={40}
@@ -36,9 +41,9 @@ const PromptCard = ({post, handleEdit, handleDelete, handleTagClick}) => {
         <div className="copy_btn" onClick={handleCopy}>
           <Image
             src={
-            copied === post.prompt
-              ? "/assets/icons/tick.svg"
-              : "/assets/icons/copy.svg"
+              copied === post.prompt
+                ? "/assets/icons/tick.svg"
+                : "/assets/icons/copy.svg"
             }
             alt={copied === post.prompt ? "tick_icon" : "copy_icon"}
             width={12}
@@ -48,9 +53,28 @@ const PromptCard = ({post, handleEdit, handleDelete, handleTagClick}) => {
       </div>
 
       <p className="my-4 font-satoshi text-sm text-gray-700">{post.prompt}</p>
-      <p className="font-inter text-sm lue_gradient cursor-pointer">{post.tag}</p>
-    </div>
-  )
-}
+      <p className="font-inter text-sm lue_gradient cursor-pointer">
+        {post.tag}
+      </p>
 
-export default PromptCard
+      {session?.user.id === post.creator._id && pathname === "/profile" && (
+        <div className="mt-5 flex-center gap-4 border-t border-gray-100 pt-3">
+          <p
+            className="font-inter text-sm green_gradient cursor-pointer"
+            onClick={handleEdit}
+          >
+            Edit
+          </p>
+          <p
+            className="font-inter text-sm orange_gradient cursor-pointer"
+            onClick={handleDelete}
+          >
+            Delete
+          </p>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default PromptCard;
